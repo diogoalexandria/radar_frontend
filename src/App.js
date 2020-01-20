480e3c56d062e3bf735a7b22e8d646256209adb0
Diogo Agra Alexandria <diogoagrasmsdc@gmail.com>
2020-01-20 20:58:31 -0300

    Criação da estilização

--- src/App.js
+++ src/App.js
@@ -1,15 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './global.css'
+import './App.js'
+import './Sidebar.css'
+import './Main.css'
 
 function App() {
   return (
     <div id="app">
        <aside>
-
+          <strong>Cadastrar</strong>
+          <form>
+            <div className="input-block">
+              <label htmlFor="github_username">Usuário do Github</label>
+              <input name="github_username" id="github_username" required/>              
+            </div>
+            <div className="input-block">
+              <label htmlFor="techs">Tecnologias</label>
+              <input name="techs" id="techs" required/>
+            </div>
+            <div className="input-group">
+              <div className="input-block">
+                <label htmlFor="latitude">Latitude</label>
+                <input name="latitude" id="latitude" required/>
+              </div>
+              <div className="input-block">
+                <label htmlFor="longitude">Latitude</label>
+                <input name="longitude" id="longitude" required/>
+              </div>
+            </div>
+            <button type="submit">Salvar</button>
+          </form>
        </aside>
        <main>
-         
+          <ul>
+            <li className="dev-item">
+              <header>
+                <img src="https://avatars1.githubusercontent.com/u/42212100?s=460&v=4" alt="imagem github"/>
+                <div className="user-info">
+                  <strong>Diogo Agra Alexandria</strong>
+                  <span>Lorem Ipsum</span>
+                </div>
+              </header>
+              <p>Etiam a ultrices arcu. Donec molestie lobortis urna, non cursus tellus accumsan a. Nam varius sodales arcu nec sagittis. Suspendisse dignissim elit dapibus tincidunt laoreet. Maecenas ac turpis mauris. Morbi in volutpat lacus. Nam porttitor quam eu nunc lobortis, vel laoreet leo molestie. </p>              
+              <a href="https://github.com/diogoalexandria">Acessar perfil do github</a>
+            </li>
+            <li className="dev-item">
+              <header>
+                <img src="https://avatars1.githubusercontent.com/u/42212100?s=460&v=4" alt="imagem github"/>
+                <div className="user-info">
+                  <strong>Diogo Agra Alexandria</strong>
+                  <span>Lorem Ipsum</span>
+                </div>
+              </header>
+              <p>Etiam a ultrices arcu. Donec molestie lobortis urna, non cursus tellus accumsan a. Nam varius sodales arcu nec sagittis. Suspendisse dignissim elit dapibus tincidunt laoreet. Maecenas ac turpis mauris. Morbi in volutpat lacus. Nam porttitor quam eu nunc lobortis, vel laoreet leo molestie. </p>              
+              <a href="https://github.com/diogoalexandria">Acessar perfil do github</a>
+            </li>
+            <li className="dev-item">
+              <header>
+                <img src="https://avatars1.githubusercontent.com/u/42212100?s=460&v=4" alt="imagem github"/>
+                <div className="user-info">
+                  <strong>Diogo Agra Alexandria</strong>
+                  <span>Lorem Ipsum</span>
+                </div>
+              </header>
+              <p>Etiam a ultrices arcu. Donec molestie lobortis urna, non cursus tellus accumsan a. Nam varius sodales arcu nec sagittis. Suspendisse dignissim elit dapibus tincidunt laoreet. Maecenas ac turpis mauris. Morbi in volutpat lacus. Nam porttitor quam eu nunc lobortis, vel laoreet leo molestie. </p>              
+              <a href="https://github.com/diogoalexandria">Acessar perfil do github</a>
+            </li>
+            <li className="dev-item">
+              <header>
+                <img src="https://avatars1.githubusercontent.com/u/42212100?s=460&v=4" alt="imagem github"/>
+                <div className="user-info">
+                  <strong>Diogo Agra Alexandria</strong>
+                  <span>Lorem Ipsum</span>
+                </div>
+              </header>
+              <p>Etiam a ultrices arcu. Donec molestie lobortis urna, non cursus tellus accumsan a. Nam varius sodales arcu nec sagittis. Suspendisse dignissim elit dapibus tincidunt laoreet. Maecenas ac turpis mauris. Morbi in volutpat lacus. Nam porttitor quam eu nunc lobortis, vel laoreet leo molestie. </p>              
+              <a href="https://github.com/diogoalexandria">Acessar perfil do github</a>
+            </li>
+          </ul>
        </main>
     </div>
   );
